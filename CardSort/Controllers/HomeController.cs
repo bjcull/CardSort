e19bb5a9ca10740ba8c75629c85750afb85efe72
Ben Cull <bjcull@gmail.com>
2016-04-21 22:14:25 +0200

    Added person cluster analysis

--- CardSort/Controllers/HomeController.cs
+++ CardSort/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Mapping;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -156,6 +158,79 @@ namespace CardSort.Controllers
                 streamWriter.BaseStream.Position = 0;
                 return File(streamWriter.BaseStream, "text/csv", string.Format("ClusterAnalysis_{0}.csv", DateTime.Now.ToString("yyyy-MM-dd_hhmmss")));
             }            
+        }
+
+        public ActionResult PersonClusters()
+        {
+            var memoryStream = new MemoryStream();
+            var streamWriter = new StreamWriter(memoryStream);
+
+            using (var db = new CardSortEntities())
+            {
+                var csv = new CsvWriter(streamWriter);
+
+                var allStrategies = db.Strategies.ToList();
+                var participants = db.Participants.AsQueryable()
+                    .Include(x => x.Categories.Select(y => y.Strategies))
+                    .ToList();
+                
+                //Write Headers
+                csv.WriteField("Participant_ID");
+                for (int y = 0; y < allStrategies.Count; y++)
+                {
+                    for (int x = y + 1; x < allStrategies.Count; x++)
+                    {
+                        csv.WriteField($"{allStrategies[y].Id}_{allStrategies[x].Id}");
+                    }
+                }
+                csv.NextRecord();
+
+                for (int p = 0; p < participants.Count; p++)
+                {
+                    csv.WriteField(participants[p].UniqueId);
+
+                    var dict = new List<string>();
+
+                    for (int c = 0; c < participants[p].Categories.Count; c++)
+                    {
+                        var category = participants[p].Categories.ElementAt(c);
+
+                        var categoryStrategies = category.Strategies.OrderBy(x => x.Id).ToList();
+
+                        for (int s = 0; s < categoryStrategies.Count; s++)
+                        {
+                            var strategy = categoryStrategies.ElementAt(s);
+                                
+                            for (int s2 = s + 1; s2 < categoryStrategies.Count; s2++)
+                            {
+                                var otherStrategy = categoryStrategies.ElementAt(s2);
+
+                                if (strategy.Id != otherStrategy.Id)
+                                {
+                                    dict.Add($"{strategy.Id}_{otherStrategy.Id}");
+                                }
+                            }
+                        }
+                    }
+
+                    for (int y = 0; y < allStrategies.Count; y++)
+                    {
+                        for (int x = y + 1; x < allStrategies.Count; x++)
+                        {
+                            var result = dict.Contains($"{allStrategies[y].Id}_{allStrategies[x].Id}");
+                            csv.WriteField(result ? 1 : 0);
+                        }
+                    }
+
+                    csv.NextRecord();
+                }
+
+                streamWriter.Flush();
+
+                // "return File" automatically disposes the stream and writer.
+                streamWriter.BaseStream.Position = 0;
+                return File(streamWriter.BaseStream, "text/csv", string.Format("PersonClusterAnalysis_{0}.csv", DateTime.Now.ToString("yyyy-MM-dd_hhmmss")));
+            }
         }
     }
 }
